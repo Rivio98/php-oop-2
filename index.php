9ce2d2acee3ecaa32de28371548c3a444777fbc7
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-18 15:56:30 +0200

    [UPD] ADd class and start constructor

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+//classe che rappresenta la categoria (cane,gatto)
+class Category
+{
+    public $name;
+    public $icon;
+
+    //Costrutto per inizializzare nome e icona 
+    public function __construct($name, $icon)
+    {
+        $this->name = $name;
+        $this->icon = $icon;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
